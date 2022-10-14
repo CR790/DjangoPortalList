b00509dddc77980d80958ad300723da59ccf6e4d
CR790 <cristianrenz56@gmail.com>
2022-10-14 02:18:02 -0300

    Solución a circule import error

--- PortalList/administracion/models.py
+++ PortalList/administracion/models.py
@@ -1,6 +1,5 @@
 from enum import unique
 from django.db import models
-from alumnos.models import Alumno
 from profesores.models import Lista, Profesor
 from django.forms import ModelForm
 # Create your models here.
@@ -15,7 +14,7 @@ class Administrador(models.Model):
     def __str__(self):
         return self.codAdministrador
 
-'''
+
 class Usuario(models.Model):
     cedula = models.IntegerField(primary_key=True, null=False)
     nombre = models.CharField(max_length=255, null=False)
@@ -26,19 +25,19 @@ class Usuario(models.Model):
     
     def __str__(self):
         return self.cedula
-'''
+
 
 class Grupo(models.Model):
     codGrupo = models.IntegerField(primary_key=True, null=False)
     nombre = models.CharField(max_length=255, null=False)
-    alumnos = models.ManyToManyField(Alumno)
+    alumnos = models.ManyToManyField(to='alumnos.Alumno')
 
     def __str__(self):
         return self.codGrupo
 
 class Estan(models.Model):
     codGrupo = models.ForeignKey(Grupo, on_delete=models.CASCADE, null=False)
-    codAlumno = models.ForeignKey(Alumno, on_delete=models.CASCADE, null=False)
+    codAlumno = models.ForeignKey(to='alumnos.Alumno', on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return self.dept_id
